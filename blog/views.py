77eb0ac8b026853d6a89ee133c5edb15e1afeeca
Rosalie <Rosalie@MacBook-Pro-de-Rosalie.local>
2015-11-28 17:41:44 +0100

    supprim print

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.all()
-    print(posts)
     return render(request, 'blog/post_list.html', {'posts': posts})
-
-
-# Create your views here.
