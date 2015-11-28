cce6fff1075d621e5fe51e8402c4657853bb4391
Rosalie <Rosalie@MacBook-Pro-de-Rosalie.local>
2015-11-28 17:37:34 +0100

    premier commit

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    print(posts)
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+# Create your views here.
